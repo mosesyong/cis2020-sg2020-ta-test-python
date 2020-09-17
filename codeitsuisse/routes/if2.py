36216d4169b5a24753412b6522151d58231a8a89
moses <moses_yong@yahoo.com.sg>
2020-09-17 16:09:18 +0800

    updated endpoint

--- codeitsuisse/routes/if2.py
+++ codeitsuisse/routes/if2.py
@@ -7,7 +7,7 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-@app.route('/intelligent-farming-2', methods=['GET'])
+@app.route('/intelligent-farming', methods=['POST'])
 def evaluateIf2():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
