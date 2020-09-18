c49d365c601e3bb1242ba588fd13cecfb3ca842e
moses <moses_yong@yahoo.com.sg>
2020-09-18 11:23:08 +0800

    create boredsc

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.if2
-
+import codeitsuisse.routes.boredSc
 
 
---
+++ codeitsuisse/routes/boredSc.py
@@ -0,0 +1,18 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/bored-scribe', methods=['POST'])
+def evaluateBoredScribe():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    logging.info("My result :{}".format(data))
+    return json.dumps(result);
+
+
+
